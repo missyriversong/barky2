369aca907f6fd3db1b1ddb9ff1b8b0795fe13871
Melissa <hibnermm@gmail.com>
2024-04-13 17:18:15 -0700

    add and test api

--- rest_tutorial/tutorial/quickstart/views.py
+++ rest_tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import permissions, viewsets
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
+from django.contrib.auth.models import Group, User
 # Create your views here.
+
+#viewset - type of class based view, no .get() .post(), instead .list() .create()
+#modelviewset - subset of viewset, with common corresponding crud 
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all().order_by('-date_joined')   #orm
+  serializer_class = UserSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+  queryset = Group.objects.all().order_by('name')   
+  serializer_class = GroupSerializer
+  permission_classes = [permissions.IsAuthenticated]
